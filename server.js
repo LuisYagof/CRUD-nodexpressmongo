bf8b44fe5e2f0736907121cf6e6c8d6dc771c06d
LuisYagof <luisyagofdez@gmail.com>
2021-03-17 22:58:05 +0100

    FEAT: add update function from front to database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ENV = require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient
 const MONGOdb = process.env.MONGO
+const optionsMongo = { useNewUrlParser: true, useUnifiedTopology: true } 
 
 // { useUnifiedTopology: true } ???? 
 
@@ -20,14 +21,13 @@ server.listen(listenPort,
 
 server.get('/books', (req, res) => {
     if (!req.query.title) {
-        MongoClient.connect(MONGOdb, (err, db) => {
+        MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
             if (err) throw err;
             db.db("CRUDapp")
                 .collection("books")
                 .find({}, {projection: {title:1, authors:1}})
                 .toArray((err, result) => {
                     if (err) throw err;
-                    console.log(result);
                     res.send(result)
                     db.close();
                 });
@@ -35,13 +35,12 @@ server.get('/books', (req, res) => {
     } else if (req.query.title) {
         let query = {title: req.query.title}
 
-        MongoClient.connect(MONGOdb, (err, db) => {
+        MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
             if (err) throw err;
             let dbo = db.db("CRUDapp");
             dbo.collection("books")
                 .findOne(query, {projection: {_id:0, title:1, authors:1}}, function(err, result) {
                 if (err) throw err;
-                console.log(result);
                 res.send(result)
                 db.close();
             });
@@ -53,13 +52,12 @@ server.get('/books', (req, res) => {
 
 server.post('/books/create', async (req, res) => {
     let newBook = req.body
-    await MongoClient.connect(MONGOdb, (err, db) => {
+    await MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
         if (err) throw err;
         db.db("CRUDapp")
             .collection("books")
             .insertOne(newBook, (err, result) => {
                 if (err) throw err;
-                // console.log(result);
                 res.send("Book was added correctly")
                 db.close();
             });
@@ -67,17 +65,52 @@ server.post('/books/create', async (req, res) => {
 })
 
 // -----------------------------------------PETICIÓN DELETE
+// ***** NO FUNCIONA CON QUERY PARAMS. SÓLO QUERYSTRINGS
 
-server.delete('/books/delete', async (req, res) => {
-    let deleted = req.body
-    await MongoClient.connect(MONGOdb, (err, db) => {
+// server.delete('/books/delete/:id', (req, res) => {
+//     let deleted = req.params.id
+//     console.log(deleted);
+//     MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
+//         if (err) throw err;
+//         db.db("CRUDapp")
+//             .collection("books")
+//             .deleteOne({_id: deleted}, (err, result) => {
+//                 if (err) throw err;
+//                 res.send("Book was deleted correctly")
+//                 db.close();
+//             });
+//     })
+// })
+
+server.delete('/books/delete', (req, res) => {
+    console.log(req.query);
+    let deleted = { title: req.query.title }
+    console.log(deleted);
+    MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
         if (err) throw err;
         db.db("CRUDapp")
             .collection("books")
             .deleteOne(deleted, (err, result) => {
                 if (err) throw err;
-                // console.log(result);
                 res.send("Book was deleted correctly")
+                db.close();
+            });
+    })
+})
+
+// -----------------------------------------PETICIÓN EDIT
+
+server.put('/books/edit', (req, res) => {
+    console.log(req.query);
+    let edited = { title: req.query.title }
+    let newVal = {$set: req.body }
+    MongoClient.connect(MONGOdb, optionsMongo, (err, db) => {
+        if (err) throw err;
+        db.db("CRUDapp")
+            .collection("books")
+            .updateOne(edited, newVal, (err, result) => {
+                if (err) throw err;
+                res.send("Book was edited correctly")
                 db.close();
             });
     })
